7be1763f7a5164ab95e2b4846f1d7d68dab37647
Anuj Mishra <anujmishra282003@gmail.com>
2023-02-28 00:09:39 +0530

    bii

--- index.py
+++ index.py
@@ -5,12 +5,6 @@ import os
 def main():
     st.title("YouTube Video Downloader")
 
-    # Determine the download folder based on the user's operating system
-    if os.name == "nt": # Windows OS
-        DOWNLOAD_FOLDER = f"{os.path.expanduser('~')}\\Downloads"
-    else: # Linux/Mac OS
-        DOWNLOAD_FOLDER = f"{os.path.expanduser('~')}/Downloads"
-
     # Get the YouTube video URL from the user
     video_url = st.text_input("Enter the YouTube video URL:")
 
@@ -23,15 +17,22 @@ def main():
             # Get the highest resolution video stream
             stream = yt.streams.get_highest_resolution()
 
-            # Download the video to the download folder
-            stream.download(output_path=DOWNLOAD_FOLDER)
+            # Ask the user to select the download directory using the system's file explorer
+            download_dir = st.text_input("Enter the download directory:")
+            if not download_dir:
+                download_dir = os.path.expanduser("~") + os.sep + "Downloads"
+            else:
+                download_dir = os.path.abspath(download_dir)
 
-            # Show success message to user
-            st.success("Video downloaded successfully! Check your Downloads folder.")
+            if not os.path.exists(download_dir):
+                st.error(f"The directory {download_dir} does not exist.")
+                return
 
-        except Exception as e:
-            # Show error message to user
-            st.error(f"Oops! Something went wrong. Error message: {e}")
+            # Download the video to the selected directory
+            file_path = stream.download(download_dir)
+            st.success(f"Video downloaded successfully at {file_path}")
+        except:
+            st.error("Oops! Something went wrong. Please check the video URL and try again.")
 
 if __name__ == '__main__':
     main()
