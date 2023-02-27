174fc32014c380a665f2c6ff6d198c2d27947ba9
Anuj Mishra <anujmishra282003@gmail.com>
2023-02-27 23:04:18 +0530

    index

---
+++ index.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from pytube import YouTube
+import os
+def main():
+    st.title("YouTube Video Downloader")
+    if os.name == "nt":
+        DOWNLOAD_FOLDER = f"{os.getenv('USERPROFILE')}\\Downloads"
+    else:  # PORT: For *Nix systems
+        DOWNLOAD_FOLDER = f"{os.getenv('HOME')}/Downloads"
+    # Get the YouTube video URL from the user
+    video_url = st.text_input("Enter the YouTube video URL:")
+
+    # Download the YouTube video when the user clicks the "Download" button
+    if st.button("Download"):
+        try:
+            # Create a YouTube object from the video URL
+            yt = YouTube(video_url)
+
+            # Get the highest resolution video stream
+            stream = yt.streams.get_highest_resolution()
+
+            # Download the video to the current directory
+            stream.download(DOWNLOAD_FOLDER)
+            st.success("Video downloaded successfully!")
+        except:
+            st.error("Oops! Something went wrong. Please check the video URL and try again.")
+
+if __name__ == '__main__':
+    main()
