bbb8c15cd6a5767f2287a080239b596b130357ab
Anuj Mishra <anujmishra282003@gmail.com>
2023-02-28 22:03:52 +0530

    lkml;

--- index.py
+++ index.py
@@ -1,26 +1,37 @@
 import streamlit as st
 from pytube import YouTube
-import pytube
 import os
-from flask import Flask, send_file,request
-import requests
 
-app = Flask(__name__)
-
-@app.route('/download')
 def main():
     st.title("YouTube Video Downloader")
+
+    # Determine the download folder based on the user's operating system
+    if os.name == "nt": # Windows OS
+        DOWNLOAD_FOLDER = f"{os.path.expanduser('~')}\\Downloads"
+    else: # Linux/Mac OS
+        DOWNLOAD_FOLDER = f"{os.path.expanduser('~')}/Downloads"
+
+    # Get the YouTube video URL from the user
     video_url = st.text_input("Enter the YouTube video URL:")
 
+    # Download the YouTube video when the user clicks the "Download" button
     if st.button("Download"):
-        # Send a request to the Flask endpoint to download the video
-        url = f"http://localhost:5000/download?video_url={video_url}"
-        response = requests.get(url)
+        try:
+            # Create a YouTube object from the video URL
+            yt = YouTube(video_url)
 
-        if response.status_code == 200:
-            st.success("Video downloaded successfully!")
-        else:
-            st.error("Oops! Something went wrong. Please try again.")
+            # Get the highest resolution video stream
+            stream = yt.streams.get_highest_resolution()
+
+            # Download the video to the download folder
+            stream.download(output_path=DOWNLOAD_FOLDER)
+
+            # Show success message to user
+            st.success("Video downloaded successfully! Check your Downloads folder.")
+
+        except Exception as e:
+            # Show error message to user
+            st.error(f"Oops! Something went wrong. Error message: {e}")
 
 if __name__ == '__main__':
     main()
