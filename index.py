4a3ce89522e695ca8ca4484c1ff183cc27eecd39
Anuj Mishra <anujmishra282003@gmail.com>
2023-02-28 22:01:34 +0530

    knlkr

--- index.py
+++ index.py
@@ -8,28 +8,13 @@ import requests
 app = Flask(__name__)
 
 @app.route('/download')
-def download():
-    # Get the YouTube video URL from the user
-    video_url = request.args.get('video_url')
-
-    # Create a YouTube object from the video URL
-    yt = YouTube(video_url)
-
-    # Get the highest resolution video stream
-    stream = yt.streams.get_highest_resolution()
-
-    # Set the Content-Disposition header to specify the filename and download behavior
-    response = send_file(stream.download(), as_attachment=True, attachment_filename=yt.title+".mp4")
-
-    return response
-
 def main():
     st.title("YouTube Video Downloader")
     video_url = st.text_input("Enter the YouTube video URL:")
 
     if st.button("Download"):
         # Send a request to the Flask endpoint to download the video
-        url = f"http://localhost:5050/download?video_url={video_url}"
+        url = f"http://localhost:5000/download?video_url={video_url}"
         response = requests.get(url)
 
         if response.status_code == 200:
@@ -38,4 +23,4 @@ def main():
             st.error("Oops! Something went wrong. Please try again.")
 
 if __name__ == '__main__':
-    app.run()
+    main()
