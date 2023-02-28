be8a32c28743b63a7ddddc63937a541dbd442ec2
Anuj Mishra <anujmishra282003@gmail.com>
2023-02-28 21:26:19 +0530

    kjnk

--- index.py
+++ index.py
@@ -1,22 +1,13 @@
 import streamlit as st
 from pytube import YouTube
+import pytube
 import os
-import tkinter as tk
-from tkinter import filedialog
 
 def main():
     st.title("YouTube Video Downloader")
-
+    DOWNLOAD_FOLDER = f"{os.getenv('HOME')}/Downloads"
     # Get the YouTube video URL from the user
     video_url = st.text_input("Enter the YouTube video URL:")
-
-    # Get the download directory from the user
-    download_dir = st.button("Select download directory")
-    if download_dir:
-        root = tk.Tk()
-        root.withdraw()
-        download_dir = filedialog.askdirectory()
-        st.write(f"Selected directory: {download_dir}")
 
     # Download the YouTube video when the user clicks the "Download" button
     if st.button("Download"):
@@ -27,11 +18,13 @@ def main():
             # Get the highest resolution video stream
             stream = yt.streams.get_highest_resolution()
 
-            # Download the video to the selected directory
-            stream.download(download_dir)
+            # Download the video to the current directory
+            stream.download(DOWNLOAD_FOLDER)
             st.success("Video downloaded successfully!")
-        except:
-            st.error("Oops! Something went wrong. Please check the video URL and try again.")
+        except pytube.exceptions.VideoUnavailable:
+            st.error("Oops! Video is unavailable. Please check the video URL and try again.")
+        except Exception as e:
+            st.error(f"Oops! Something went wrong. Error message: {e}")
 
 if __name__ == '__main__':
     main()
